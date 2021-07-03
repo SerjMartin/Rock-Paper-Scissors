92a37b24a8078a126809327f89de3309394c0302
SerjMartin <serjmartin@mail.com>
2021-07-03 17:39:30 +0000

    Add fuction checkAnswer

--- accets/js/script.js
+++ accets/js/script.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoadet", function () {
-    let button = document.getElementsByTagName("button");
+    let buttons = document.getElementsByTagName("button");
     for (let button of buttons) {
         button.addEventListener("click", function () {
             if (this.getAttribute("data-type") === "submit") {
-                checkAnswer();
+                comChoise();
             }
         })
     }
@@ -14,9 +14,10 @@ let object = ['<i id="icon" class="fas fa-hand-rock"></i>', '<i id="icon" class=
 let guest = document.getElementById("guest");
 let compChoice = document.getElementById("computer");
 
+function comChoise() {
     index = Math.floor(Math.random() * 3);
     compChoice.innerHTML = object[index];
-
+}
 
 function guestChoice() {
     guest.innerHTML = '<i id="icon" class="fas fa-hand-rock"></i>';
