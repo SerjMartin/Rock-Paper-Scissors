61e9043b332da678d18c9057cde2507ab8c718f3
SerjMartin <serjmartin@mail.com>
2021-07-05 15:01:40 +0000

    Add function reset game

--- accets/js/script.js
+++ accets/js/script.js
@@ -13,25 +13,68 @@ document.addEventListener("DOMContentLoadet", function () {
 let object = ['<i id="icon" class="fas fa-hand-rock"></i>', '<i id="icon" class="fas fa-hand-scissors"></i>', '<i id="icon" class="fas fa-hand-paper"></i>'];
 let guest = document.getElementById("guest");
 let compChoice = document.getElementById("computer");
+let result = document.getElementById("resultP");
 
 function comChoise() {
     index = Math.floor(Math.random() * 3);
     compChoice.innerHTML = object[index];
 }
 
+let rock = object[0];
+let scissors = object[1];
+let paper = object[2];
+
 function guestChoice() {
-    guest.innerHTML = '<i id="icon" class="fas fa-hand-rock"></i>';
+    guest.innerHTML = rock;
 }
 
 function guestChoiceA() {
-    guest.innerHTML = '<i id="icon" class="fas fa-hand-paper"></i>';
+    guest.innerHTML = paper;
 }
 
 function guestChoiceB() {
-    guest.innerHTML = '<i id="icon" class="fas fa-hand-scissors"></i>';
+    guest.innerHTML = scissors;
+}
+
+function resetGame() {
+    compChoice.innerHTML = "";
+    result.innerHTML = "";
 }
 
 
 function checkAnswer() {
+       if (guest.innerHTML == rock && compChoice.innerHTML == paper) {
+           result.innerText= "You Lost!!";
+           incrementWrongScore();
+       }  else if (guest.innerHTML == scissors && compChoice.innerHTML == rock) {
+           result.innerText= "You Lost!!";
+           incrementWrongScore();
+       } else if (guest.innerHTML == paper && compChoice.innerHTML == scissors) {
+          result.innerText= "You Lost!!";
+          incrementWrongScore();
+       } else if (guest.innerHTML == scissors && compChoice.innerHTML == paper) {
+            result.innerText= "You Won!!";
+            incrementScore();
+       } else if (guest.innerHTML == rock && compChoice.innerHTML == scissors) {
+            result.innerText= "You Won!!";
+            incrementScore();
+       } else if (guest.innerHTML == paper && compChoice.innerHTML == rock) {
+            result.innerText= "You Won!!";
+            incrementScore();
+       }  else if (guest.innerHTML == compChoice.innerHTML) {
+        result.innerText= "It's tie!!";
+       } else { 
+        result.innerText= "Make your choise!";
+       }
 
+}
+
+function incrementScore() {
+ let oldscore = parseInt(document.getElementById('won').innerText);
+ document.getElementById("won").innerText = ++oldscore;
+}
+
+function incrementWrongScore() {
+   let oldscore = parseInt(document.getElementById("lost").innerText);
+   document.getElementById("lost").innerText = ++oldscore;
 }
